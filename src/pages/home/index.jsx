0e69cf6353c74ca15cb6d91ac8351df877bf26a5
Ayoubbezai2024 <158614538+AYOUBBB123@users.noreply.github.com>
2024-12-17 11:27:15 +0100

    Update index.jsx

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,18 +3,25 @@ import { GlobalContext } from '../../context';
 import RecipeItem from "../../components/recipe-item";
 
 export default function Home() {
-    const { recipeList, loading } = useContext(GlobalContext)
+    const { recipeList, loading, handlerandomrecipeList } = useContext(GlobalContext)
 
     if (loading) return <div> Loading ... Please wait!!</div>
     return <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10 ">
         {
             recipeList && recipeList.length > 0 ?
                 recipeList.map((item) => <RecipeItem item={item} />)
-                : <div>
+                : <div className="flex flex-col gap-8  justify-center">
                     <p
                         className="lg:text-4xl text-xl text-center text-black font-extrabold"
                     >Nothing to show . Please search for somthine </p>
+
+                    <button onClick={() => handlerandomrecipeList()} className=" p-3 px-8 rounded-lg text-sm uppercase  font-medium tracking-wider mt-3 inline-block shadow-md bg-black text-white "> Random </button>
+                    <div>
+                        {!loading && "Click on random to genertae "}
+                    </div>
+
                 </div>
+
         }
 
     </div>
