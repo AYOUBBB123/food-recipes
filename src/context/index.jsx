44c07697d2e14a50c001d70dcf46f162b3e5cfe2
Ayoubbezai2024 <158614538+AYOUBBB123@users.noreply.github.com>
2024-12-17 11:27:51 +0100

    Update index.jsx

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -4,6 +4,26 @@ import { useNavigate } from 'react-router-dom';
 export const GlobalContext = createContext(null);
 
 export default function GlobalState({ children }) {
+    const items = [
+        "carrot", "broccoli", "asparagus", "cauliflower", "corn", "cucumber", "green pepper",
+        "lettuce", "mushrooms", "onion", "potato", "pumpkin", "red pepper", "tomato",
+        "beetroot", "brussel sprouts", "peas", "zucchini", "radish", "sweet potato",
+        "artichoke", "leek", "cabbage", "celery", "chili", "garlic", "basil", "coriander",
+        "parsley", "dill", "rosemary", "oregano", "cinnamon", "saffron", "green bean",
+        "bean", "chickpea", "lentil", "apple", "apricot", "avocado", "banana",
+        "blackberry", "blackcurrant", "blueberry", "boysenberry", "cherry", "coconut",
+        "fig", "grape", "grapefruit", "kiwifruit", "lemon", "lime", "lychee", "mandarin",
+        "mango", "melon", "nectarine", "orange", "papaya", "passion fruit", "peach",
+        "pear", "pineapple", "plum", "pomegranate", "quince", "raspberry", "strawberry",
+        "watermelon", "salad", "pizza", "pasta", "popcorn", "lobster", "steak", "bbq",
+        "pudding", "hamburger", "pie", "cake", "sausage", "tacos", "kebab", "poutine",
+        "seafood", "chips", "fries", "masala", "paella", "som tam", "chicken", "toast",
+        "marzipan", "tofu", "ketchup", "hummus", "chili", "maple syrup", "parma ham",
+        "fajitas", "champ", "lasagna", "poke", "chocolate", "croissant", "arepas",
+        "bunny chow", "pierogi", "donuts", "rendang", "sushi", "ice cream", "duck",
+        "curry", "beef", "goat", "lamb", "turkey", "pork", "fish", "crab", "bacon",
+        "ham", "pepperoni", "salami", "ribs"
+    ];
 
     const [searchParam, setsearchParam] = useState("")
     const [loading, setloading] = useState(false)
@@ -11,7 +31,26 @@ export default function GlobalState({ children }) {
     const [recipeDetailsData, setrecipeDetailsData] = useState(null)
     const [favoritesList, setfavoritesList] = useState([])
 
+    async function handlerandomrecipeList() {
+        const randomElement = items[Math.floor(Math.random() * items.length)];
+        console.log(randomElement);
 
+        try {
+            const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${randomElement}`)
+            const data = await res.json();
+            if (data?.data?.recipes) {
+                setrecipelist(data?.data?.recipes)
+                setloading(false)
+                setsearchParam("")
+            }
+
+
+        } catch (e) {
+            console.log(e);
+            setloading(false)
+            setsearchParam("")
+        }
+    }
     function handlesearchParam() {
         setrecipelist({})
         setloading(false)
@@ -56,5 +95,5 @@ export default function GlobalState({ children }) {
     }
 
     console.log(loading, recipeList)
-    return <GlobalContext.Provider value={{ searchParam, loading, recipeList, setsearchParam, handleSubmit, handlesearchParam, recipeDetailsData, setrecipeDetailsData, handlefavoritesList, favoritesList }}>{children}</GlobalContext.Provider>
+    return <GlobalContext.Provider value={{ searchParam, loading, recipeList, setsearchParam, handleSubmit, handlesearchParam, recipeDetailsData, setrecipeDetailsData, handlefavoritesList, favoritesList, handlerandomrecipeList }}>{children}</GlobalContext.Provider>
 }
