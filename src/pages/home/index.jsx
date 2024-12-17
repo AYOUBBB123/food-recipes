c8f923dc65a73cbb7359e158fa6ec87269c075dc
Ayoubbezai2024 <158614538+AYOUBBB123@users.noreply.github.com>
2024-12-17 22:51:53 +0100

    Update index.jsx

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -6,23 +6,21 @@ export default function Home() {
     const { recipeList, loading, handlerandomrecipeList } = useContext(GlobalContext)
 
     if (loading) return <div> Loading ... Please wait!!</div>
-    return <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10 ">
+    return <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10 "> <div className="w-full flex justify-center"> <button onClick={() => handlerandomrecipeList()} className=" p-3 px-6  rounded-lg text-sm uppercase  font-medium tracking-wider mt-3 inline-block shadow-md bg-black text-white  "> Random </button> </div>
+
         {
             recipeList && recipeList.length > 0 ?
                 recipeList.map((item) => <RecipeItem item={item} />)
-                : <div className="flex flex-col gap-8  justify-center">
+                : <div className="flex flex-col gap-8  justify-center text-center ">
                     <p
                         className="lg:text-4xl text-xl text-center text-black font-extrabold"
                     >Nothing to show . Please search for somthine </p>
 
-                    <button onClick={() => handlerandomrecipeList()} className=" p-3 px-8 rounded-lg text-sm uppercase  font-medium tracking-wider mt-3 inline-block shadow-md bg-black text-white "> Random </button>
-                    <div>
-                        {!loading && "Click on random to genertae "}
-                    </div>
 
                 </div>
 
         }
+
 
     </div>
 }
